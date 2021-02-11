eac21f61ea8815c41e5b290ccf55ec9bba95229b
Luke Travis <lukehtravis@gmail.com>
2021-02-11 11:26:04 -0800

    modify db config for prod

--- startup/dbconfig.js
+++ startup/dbconfig.js
@@ -1,10 +1,22 @@
 const Pool = require("pg").Pool;
-console.log(process.env);
-const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: "randomactsofchristmas",
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
+
+const isProduction = process.env.NODE_ENV === "production";
+
+let pool = {};
+
+if (!isProduction) {
+  pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: "randomactsofchristmas",
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction,
+  });
+}
+
 exports.pool = pool;
