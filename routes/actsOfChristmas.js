7c13137536c421fefad0214ba91331381d96d641
Luke Travis <lukehtravis@gmail.com>
2021-02-14 20:08:48 -0800

    add allowance for cors requests

--- routes/actsOfChristmas.js
+++ routes/actsOfChristmas.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const winston = require("winston");
 const router = express.Router();
 const {
   actOfChristmas,
   createActOfChristmas,
 } = require("../models/actOfChristmas");
+
+app.use(cors());
 
 router.get("/", actOfChristmas);
 
